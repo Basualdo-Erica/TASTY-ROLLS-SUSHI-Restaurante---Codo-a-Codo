fb16323c3c05a53e7edc6123c678147c24f50255
Erica Basualdo <ericabasualdo682@gmail.com>
2024-05-15 14:57:13 -0300

    ningun commits

--- index.js
+++ index.js
@@ -19,21 +19,39 @@
 // }
 
 // menu de navegacion con scroll hacia abajo
-window.addEventListener('scroll', function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("abajo", window.scrollY>0);
-})
+// document.addEventListener("DOMContentLoaded", function() {
+//     const burger = document.getElementById('burger');
+//     const navbar = document.querySelector('.navbar-a-visible');
 
+//     burger.addEventListener('click', function() {
+//         navbar.classList.toggle('active');
+//     });
+// });
 
-
-
+// window.addEventListener('scroll', function() {
+//     var header = document.querySelector("header");
+//     header.classList.toggle("abajo", window.scrollY > 0);
+// });
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    const logoMenu = document.querySelector('.bx-align-right');
-    const menuUl = document.querySelector('.menu ul');
+    const burger = document.getElementById('burger');
+    const navbar = document.querySelector('.navbar-a-visible');
+    const logo = document.getElementById('logo');
 
-    logoMenu.addEventListener('click', function() {
-        menuUl.classList.toggle('menu-ul-visible');
+    if (burger && navbar) {
+        burger.addEventListener('click', function() {
+            navbar.classList.toggle('active');
+            if (logo) {
+                logo.classList.toggle('hidden');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', function() {
+        var header = document.querySelector("header");
+        if (header) {
+            header.classList.toggle("abajo", window.scrollY > 0);
+        }
     });
 });
